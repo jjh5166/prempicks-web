5a298617ca4f140d16861b77ea94421350bb22d5
John <jjh5166@gmail.com>
2020-08-30 14:23:34 -0400

    MUI theme

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,17 @@
+import { MuiThemeProvider } from '@material-ui/core/styles';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import '../styles/globals.css';
 
+import theme from '../styles/theme';
+
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+
+  return (
+    <MuiThemeProvider theme={theme}>
+      <Component {...pageProps} />
+    </MuiThemeProvider>
+  )
+
 }
 
 export default MyApp
---
+++ styles/theme.js
@@ -0,0 +1,36 @@
+import { createMuiTheme } from '@material-ui/core/styles';
+
+const theme = createMuiTheme({
+  overrides: {
+    MuiInput: {
+      root: {
+        color: '#fff',
+      },
+      underline: {
+        "&:before": {
+          // normal
+          borderBottom: `1px solid #fff`,
+        },
+        "&:after": {
+          // focused
+          borderBottom: `2px solid #fff`
+        },
+        "&:hover:not($disabled):not($focused):not($error):before": {
+          //hover
+          borderBottom: `2px solid #fff`,
+        },
+      }
+    },
+    MuiInputLabel: {
+      root: {
+        color: '#fff',
+        "&$focused": {
+          "color": "#fff"
+        }
+      },
+
+    }
+  }
+});
+
+export default theme;
