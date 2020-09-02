f49e52655a89907b743419033addf2c38dd10a6a
John <jjh5166@gmail.com>
2020-09-01 23:20:36 -0400

    wrap app w/ Redux & Firebase providers

--- pages/_app.js
+++ pages/_app.js
@@ -1,20 +1,23 @@
+import React from 'react';
+import { Provider } from 'react-redux';
 import { MuiThemeProvider } from '@material-ui/core/styles';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import '../styles/globals.css';
 
-import Firebase, { FirebaseContext } from '../components/Firebase';
+import { useStore } from '../store';
+import { FirebaseProvider } from '../components/Firebase'
 import theme from '../styles/theme';
 
-function MyApp({ Component, pageProps }) {
+export default function App({ Component, pageProps }) {
+  const store = useStore(pageProps.initialReduxState);
 
   return (
-    <FirebaseContext.Provider value={new Firebase()}>
-      <MuiThemeProvider theme={theme}>
-        <Component {...pageProps} />
-      </MuiThemeProvider>
-    </FirebaseContext.Provider>
-  )
-
-}
-
-export default MyApp
+    <Provider store={store}>
+      <FirebaseProvider>
+        <MuiThemeProvider theme={theme}>
+          <Component {...pageProps} />
+        </MuiThemeProvider>
+      </FirebaseProvider>
+    </Provider>
+  );
+};
