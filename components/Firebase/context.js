d95f186dabacfae9b48585e12b19b7755e921e3c
John <jjh5166@gmail.com>
2020-09-01 23:17:52 -0400

    FirebaseProvider with auth listener

--- components/Firebase/context.js
+++ components/Firebase/context.js
@@ -1,6 +1,8 @@
-import React from 'react';
+import { useEffect, createContext } from 'react'
+import { useDispatch } from 'react-redux';
+import Firebase from './firebase';
 
-const FirebaseContext = React.createContext(null);
+const FirebaseContext = createContext(null);
 
 export const withFirebase = Component => props => (
   <FirebaseContext.Consumer>
@@ -8,4 +10,31 @@ export const withFirebase = Component => props => (
   </FirebaseContext.Consumer>
 );
 
-export default FirebaseContext;
+export function FirebaseProvider({ children }) {
+  const firebase = new Firebase();
+  const dispatch = useDispatch();
+  useEffect(() => {
+    // Listen authenticated user
+    const unsubscriber = firebase.auth.onAuthStateChanged(async (user) => {
+      try {
+        if (user) {
+          const { uid } = user;
+          dispatch({ type: "LOGIN", uid: uid})
+        } else { 
+          dispatch({ type: "LOGOUT" })
+        };
+      } catch (error) {
+        // Most probably a connection error. Handle appropriately.
+      }
+    });
+
+    // Unsubscribe auth listener on unmount
+    return () => unsubscriber();
+  }, []);
+
+  return (
+    <FirebaseContext.Provider value={firebase}>
+      {children}
+    </FirebaseContext.Provider>
+  );
+}
