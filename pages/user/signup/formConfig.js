d3bfdbc3344af355d6b7ee76c9c15ba0156d3eed
John <jjh5166@gmail.com>
2020-09-05 15:12:02 -0400

    sign up with API after firebase

--- pages/user/signup/formConfig.js
+++ pages/user/signup/formConfig.js
@@ -1,4 +1,6 @@
 import Router from 'next/router';
+import axios from 'axios';
+import { serverUrl } from '../../../constants';
 
 export const initialValues = {
   first_name: '',
@@ -43,10 +45,29 @@ export const signupFields = [
 ];
 
 export const signupFn = async (firebase, data) => {
+  let uid;
   await firebase.doCreateUserWithEmailAndPassword(
     data.email,
     data.password
-  ).then(res => console.log(res))
-  .catch(err => console.log(err))
+  ).then(res => {
+    uid = res.user.uid;
+  })
+    .catch(err => console.log(err));
+  await axios.post(`${serverUrl}/user`, {
+    user: {
+      uid: uid,
+      email: data.email,
+      first_name: data.first_name,
+      last_name: data.last_name,
+      team_name: data.team_name
+    },
+    idToken: await firebase.retrieveToken()
+  },
+    { headers: { 'Content-Type': 'application/json' }})
+    .then(res => {
+      console.log(res);
+    }).catch(err => {
+      console.log(err.response);
+    });
   await Router.push('/mypicks')
 };
