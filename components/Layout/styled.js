09a776b8c40dcab27278a63eaac18e2901e36e18
John <jjh5166@gmail.com>
2020-09-11 23:19:07 -0400

    rm react-div-100vh, webkit-fill-av height

--- components/Layout/styled.js
+++ components/Layout/styled.js
@@ -2,6 +2,7 @@ import styled from 'styled-components';
 
 export const PageContainer = styled.div`
   height: 100%;
+  height: -webkit-fill-available;
   width: 100%;
   display: flex;
   flex-direction: column;
