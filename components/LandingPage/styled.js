87e7224c00834d3af86cc6f52825a161c80c6c44
John <jjh5166@gmail.com>
2020-09-11 23:27:46 -0400

    Landing Page responsive styles

--- components/LandingPage/styled.js
+++ components/LandingPage/styled.js
@@ -1,9 +1,11 @@
 import styled from 'styled-components';
 import Button from 'react-bootstrap/Button';
 
+import { device } from '../../constants';
+
 export const BigTitle = styled.div`
   position: absolute;
-  top: 21%;
+  top: 29%;
   left: 50%;
   transform: translate(-50%, -50%);
   vertical-align: middle;
@@ -12,6 +14,12 @@ export const BigTitle = styled.div`
   color: rgb(31, 154, 192);
   text-shadow: 2px 2px #00394d;
   text-align: center;
+  @media (orientation: landscape) {
+    line-height: 1
+  }
+  @media ${device.tablet} {
+    line-height: 1.5;
+  }
 `;
 export const LandingButton = styled(Button)`
     display: block;
@@ -27,10 +35,16 @@ export const LandingButton = styled(Button)`
 `;
 export const LandingButtonsContainer = styled.div`
   position: absolute;
-  top: 62%;
+  top: 74%;
   left: 50%;
   transform: translate(-50%, -50%);
   width: 100%;
   display: flex;
   justify-content: center;
+  @media (orientation: landscape) {
+    top: 80%
+  }
+  @media ${device.tablet} {
+    top: 74%;
+  }
 `;
