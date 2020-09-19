4bb6656b4cbcc870504023cfe772f7207eb2ba7b
John <jjh5166@gmail.com>
2020-09-18 23:02:15 -0400

    redirect to mypicks if authUser

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,21 @@
+import { useEffect } from 'react';
+import Router from 'next/router';
+
 import LandingPage from '../components/LandingPage';
 import Layout from '../components/Layout';
+import useAuthUser from '../redux/useAuthUser';
+
 
 export default function Home() {
+  const { authUser } = useAuthUser();
+  useEffect(() => {
+    if (authUser) {
+      Router.push('/mypicks');
+    }
+  }, [authUser]);
   return (
     <Layout hideNav={true}>
       <LandingPage />
     </Layout>
   );
-}
+};
--- pages/user/login.js
+++ pages/user/login.js
@@ -1,12 +1,23 @@
-import React from 'react';
+import { useEffect } from 'react';
+import Router from 'next/router';
 
 import Layout from '../../components/Layout';
 import LoginForm from '../../components/UserForm/Login';
+import useAuthUser from '../../redux/useAuthUser';
 
-const LoginPage = () => (
-  <Layout title="Login" hideNav={true}>
-    <LoginForm />
-  </Layout>
-);
+const LoginPage = () => {
+  const { authUser } = useAuthUser();
+  useEffect(() => {
+    if (authUser) {
+      Router.push('/mypicks');
+    }
+  }, [authUser]);
+
+  return (
+    <Layout title="Login" hideNav={true}>
+      <LoginForm />
+    </Layout>
+  );
+};
 
 export default LoginPage;
