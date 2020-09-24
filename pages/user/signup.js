4dae33053eadc684d129c9e09976db452ce37d3a
John <jjh5166@gmail.com>
2020-09-23 22:42:08 -0400

    immediate redirect from signup

--- pages/user/signup.js
+++ pages/user/signup.js
@@ -1,12 +1,17 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 
 import Layout from '../../components/Layout';
 import SignUpForm from '../../components/UserForm/SignUp';
 
-const SignupPage = () => (
+const SignupPage = () => {
+  // immediate redirect to prevent sign up
+  useEffect(() => {
+    Router.push('/');
+  }, []);
+  return(
   <Layout title="Sign Up" hideNav={true}>
     <SignUpForm />
   </Layout>
-);
+);}
 
 export default SignupPage;
