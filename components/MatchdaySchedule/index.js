652202eca209ca43c16301d0fae6fd62c1a4bc58
John <jjh5166@gmail.com>
2020-11-03 11:10:13 -0500

    showScore

--- components/MatchdaySchedule/index.js
+++ components/MatchdaySchedule/index.js
@@ -6,28 +6,34 @@ import {
   MatchContainer, ScoreContainer, HomeTeam, AwayTeam
 } from './styled';
 
+function showScore(status) {
+  return status === ('FINISHED' || 'AWARDED' || 'INPLAY' || 'PAUSED');
+}
 const MatchdaySchedule = ({ matchday, matches }) => {
   return (
     <Fragment>
       <MatchdayTitle>{`Matchday ${matchday}`}</MatchdayTitle>
       <ScheduleContainer>
-          {matches.map(match => {
-            console.log(match);
-            return (
-              <MatchContainer key={match.id}>
-                <HomeTeam>
-                  <span>{teamsMap[match.homeTeam.id]['short']}</span>
-                  <img src={teamsMap[match.homeTeam.id]['crestURL']} />
-                </HomeTeam>
-                <ScoreContainer>{"vs."}</ScoreContainer>
-                <AwayTeam>
-                  <img src={teamsMap[match.awayTeam.id]['crestURL']} />
-                  <span>{teamsMap[match.awayTeam.id]['short']}</span>
-                </AwayTeam>
-              </MatchContainer>
-            );
-          }
-          )}
+        {matches.map(match => {
+          let thisScore = showScore(match.status) ?
+            `${match.score.fullTime.homeTeam} : ${match.score.fullTime.awayTeam}`
+            :
+            'vs.';
+          return (
+            <MatchContainer key={match.id}>
+              <HomeTeam>
+                <span>{teamsMap[match.homeTeam.id]['short']}</span>
+                <img src={teamsMap[match.homeTeam.id]['crestURL']} />
+              </HomeTeam>
+              <ScoreContainer>{thisScore}</ScoreContainer>
+              <AwayTeam>
+                <img src={teamsMap[match.awayTeam.id]['crestURL']} />
+                <span>{teamsMap[match.awayTeam.id]['short']}</span>
+              </AwayTeam>
+            </MatchContainer>
+          );
+        }
+        )}
       </ScheduleContainer>
     </Fragment>
   );
