6df8b19d323fac6e067e86d25740166a8ea56549
John <jjh5166@gmail.com>
2020-11-09 16:00:42 -0500

    create standingsData on guest/standings page

--- pages/guest/standings.js
+++ pages/guest/standings.js
@@ -1,14 +1,20 @@
 import React, { useState, useEffect } from 'react';
+import { useDispatch } from 'react-redux';
 
-import { fakeStandingsData } from '../../utils/guest';
+import { useGuestUser } from '../../redux/hooks';
 import Layout from '../../components/Layout';
 import StandingsTable from '../../components/Tables/userStandings';
 
 export default function GuestStandingsPage() {
+  const dispatch = useDispatch();
   const [standings, setStandings] = useState(null);
+  const { standingsData } = useGuestUser();
   useEffect(() => {
-    setStandings(fakeStandingsData());
-  }, []);
+    if(!standingsData){
+      dispatch({ type: 'CREATE_STANDINGS_DATA' });
+    }
+    setStandings(standingsData);
+  }, [standingsData]);
 
   return (
     <Layout title="Standings">
