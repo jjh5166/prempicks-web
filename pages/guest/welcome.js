7aee0ce022bb20c2c7521c15812a5eeaa2bb349d
John <jjh5166@gmail.com>
2020-11-08 23:55:19 -0500

    GuestWelcomePage with team form

--- pages/guest/welcome.js
+++ pages/guest/welcome.js
@@ -2,16 +2,17 @@ import { useEffect } from 'react';
 import { useDispatch } from 'react-redux';
 
 import Layout from '../../components/Layout';
-
+import GuestTeam from '../../components/UserForm/GuestTeam';
 
 export default function GuestWelcomePage() {
   const dispatch = useDispatch();
+
   useEffect(() => {
     dispatch({ type: "GUEST_LOGIN" });
   }, []);
   return (
-    <Layout>
-
+    <Layout hideNav={true}>
+      <GuestTeam />
     </Layout>
   );
 };
