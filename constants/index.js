6fe943c9c276aaf877460e0602ce6b96a03431de
John <jjh5166@gmail.com>
2020-11-11 22:29:10 -0500

    autopickOrder

--- constants/index.js
+++ constants/index.js
@@ -3,5 +3,9 @@ import serverUrl from './serverUrl';
 import { footballApiKey, footballApiBaseUrl } from './footballApiKey';
 import { teamsMap } from './teamsMap';
 import { teamChoices } from './teamChoices';
+import { autopickOrder } from './autopickOrder';
 
-export { device, serverUrl, teamsMap, footballApiKey, footballApiBaseUrl, teamChoices };
+export {
+  device, serverUrl, teamsMap, footballApiKey, footballApiBaseUrl,
+  teamChoices, autopickOrder
+};
