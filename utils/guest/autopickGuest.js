e7f4ff44c48d6b7c6a44ad3465593f731829d1a4
John <jjh5166@gmail.com>
2020-11-11 23:25:27 -0500

    autopickGuest

---
+++ utils/guest/autopickGuest.js
@@ -0,0 +1,40 @@
+import { autopickOrder } from '../../constants';
+
+function needsAuto(half) {
+  return !!half.filter(x => x.team_id === '').length;
+};
+function removeTeam(array, team) {
+  let rmIndex;
+  while ((rmIndex = array.indexOf(team)) > -1) {
+    array.splice(rmIndex, 1);
+  }
+}
+function autopickHalf(picks) {
+  const autos = [...autopickOrder];
+  const checkAhead = [];
+  picks.map((pick, i) => {
+    if (checkAhead.includes(pick.team_id)) {
+      let pickThis = autos.shift();
+      picks[i].team_id = pickThis;
+      checkAhead.push(pickThis);
+    }
+    if (autos.includes(pick.team_id)) {
+      removeTeam(autos, pick.team_id);
+    } else if (pick.team_id === '') {
+      let pickThis = autos.shift();
+      picks[i].team_id = pickThis;
+      checkAhead.push(pickThis);
+    }
+  });
+}
+export function autopickGuest(picks, matchday) {
+  const thesePicks = picks.slice(0, -(38 - matchday));
+  const firstHalf = thesePicks.splice(0, 19);
+  const secondHalf = thesePicks.splice(-19);
+  if (needsAuto(firstHalf)) {
+    autopickHalf(firstHalf);
+  }
+  if (needsAuto(secondHalf)) {
+    autopickHalf(secondHalf);
+  }
+}
--- utils/guest/index.js
+++ utils/guest/index.js
@@ -3,6 +3,7 @@ import { fakeStandingsData } from './fakeStandingsData';
 import { parseScheduleMyPicks } from './parseScheduleMyPicks';
 import { updateStandingsData } from './updateStandings';
 import { pluckPicks } from './pluckPicks';
+import { autopickGuest } from './autopickGuest';
 
 export { createInitialValues, fakeStandingsData, parseScheduleMyPicks,
-  updateStandingsData, pluckPicks };
+  updateStandingsData, pluckPicks, autopickGuest };
