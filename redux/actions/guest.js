f3be8eebf5634b7ea4afc2a6f9ead04a2bd5a92b
John <jjh5166@gmail.com>
2020-11-15 22:32:37 -0500

    convert guest actions to functions

--- redux/actions/guest.js
+++ redux/actions/guest.js
@@ -6,3 +6,47 @@ export const GUEST_PICKS_IN_STANDINGS = 'GUEST_PICKS_IN_STANDINGS';
 export const AUTOPICK_GUEST = 'AUTOPICK_GUEST';
 export const CHANGE_MATCHDAY = 'CHANGE_MATCHDAY';
 export const GUEST_LOGOUT = 'GUEST_LOGOUT';
+
+export const guestLogin = () => {
+  return { type: GUEST_LOGIN };
+};
+
+export const setTeam = (teamName) => {
+  return {
+    type: SET_TEAM,
+    team_name: teamName
+  };
+};
+
+export const updatePicks = (picks) => {
+  return {
+    type: UPDATE_PICKS,
+    picks: picks
+  };
+};
+
+export const createStandingsData = () => {
+  return { type: CREATE_STANDINGS_DATA };
+};
+
+export const guestPicksInStandings = () => {
+  return { type: GUEST_PICKS_IN_STANDINGS };
+};
+
+export const autoPickGuest = (matchday) => {
+  return {
+    type: AUTOPICK_GUEST,
+    matchday: parseInt(matchday)
+  };
+};
+
+export const changeMatchday = (matchday) => {
+  return {
+    type: CHANGE_MATCHDAY,
+    matchday: parseInt(matchday)
+  };
+};
+
+export const guestLogout = () => {
+  return { type: GUEST_LOGOUT };
+};
--- redux/thunks/initGuestStandings.js
+++ redux/thunks/initGuestStandings.js
@@ -1,9 +1,11 @@
+import { createStandingsData, autoPickGuest, changeMatchday } from '../actions/guest';
+
 export function initGuestStandings(matchday) {
   return async function updateMatchdayThunk(dispatch) {
     if(matchday === 0){
-      dispatch({ type: 'CHANGE_MATCHDAY', matchday: 1 });
-      dispatch({ type: 'AUTOPICK_GUEST', matchday: 1 });
+      dispatch(changeMatchday(1));
+      dispatch(autoPickGuest(1));
     }
-    dispatch({ type: 'CREATE_STANDINGS_DATA' });
+    dispatch(createStandingsData());
   };
 }
