e6143e91eef8876ec508e8ee207784d8f48fc06c
John J Hartnett V <jjh5166@gmail.com>
2020-11-16 21:13:42 -0500

    Revert "check for standingsData in updateMatchday"

--- redux/thunks/updateMatchday.js
+++ redux/thunks/updateMatchday.js
@@ -1,10 +1,9 @@
 import { guestPicksInStandings, autoPickGuest, changeMatchday } from '../actions/guest';
 
 export function updateMatchday(matchday) {
-  return async function updateMatchdayThunk(dispatch, getState) {
-    const { standingData } = getState();
+  return async function updateMatchdayThunk(dispatch) {
     dispatch(autoPickGuest(matchday));
-    if (standingData) dispatch(guestPicksInStandings());
+    dispatch(guestPicksInStandings());
     dispatch(changeMatchday(matchday));
   };
 }
