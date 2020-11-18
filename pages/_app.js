05b5467d54d420ca1685698dac41d0df1f1b619f
John <jjh5166@gmail.com>
2020-11-17 22:33:01 -0500

    setup redux-wrapper

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import { Provider } from 'react-redux';
 import { MuiThemeProvider } from '@material-ui/core/styles';
 import { config } from '@fortawesome/fontawesome-svg-core';
 import '@fortawesome/fontawesome-svg-core/styles.css'; // Import the CSS
@@ -7,20 +6,18 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import '../styles/globals.css';
 config.autoAddCss = false; // Tell Font Awesome to skip adding the CSS automatically since it's being imported above
 
-import { useStore } from '../redux/store';
+import { wrapper } from '../redux/store';
 import { FirebaseProvider } from '../components/Firebase';
 import theme from '../styles/theme';
 
-export default function App({ Component, pageProps }) {
-  const store = useStore(pageProps.initialReduxState);
-
+const WrappedApp = ({ Component, pageProps }) => {
   return (
-    <Provider store={store}>
-      <FirebaseProvider>
-        <MuiThemeProvider theme={theme}>
-          <Component {...pageProps} />
-        </MuiThemeProvider>
-      </FirebaseProvider>
-    </Provider>
+    <FirebaseProvider>
+      <MuiThemeProvider theme={theme}>
+        <Component {...pageProps} />
+      </MuiThemeProvider>
+    </FirebaseProvider>
   );
 };
+
+export default wrapper.withRedux(WrappedApp);
