f1f4752dd05370ae7e5ab45c2fc7b96fee445124
John <jjh5166@gmail.com>
2020-11-21 22:38:28 -0500

    display dates on schedule

--- components/MatchdaySchedule/index.js
+++ components/MatchdaySchedule/index.js
@@ -9,44 +9,57 @@ import {
   MatchContainer, HomeTeam, AwayTeam
 } from './styled';
 
-const MatchdaySchedule = ({ matchday, matches, changeMatchday }) =>
-  <Fragment>
-    <MatchdayTitle>
-      {
-        matchday > 1 &&
-        <FontAwesomeIcon
-          onClick={changeMatchday(matchday - 1)}
-          icon={faChevronLeft}
-          size={"lg"} />
-      }
-      <span>{`Matchday ${matchday}`}</span>
-      {
-        matchday < 38 &&
-        <FontAwesomeIcon
-          onClick={changeMatchday(matchday + 1)}
-          icon={faChevronRight}
-          size={"lg"} />
-      }
-    </MatchdayTitle>
-    <ScheduleContainer>
-      {matches.map(match =>
-        <MatchContainer key={match.id}>
-          <HomeTeam>
-            <span>{teamsMap[match.homeTeam.id]['short']}</span>
-            <img src={teamsMap[match.homeTeam.id]['crestURL']} />
-          </HomeTeam>
-          <ScoreContainer
-            home={match.score.fullTime.homeTeam}
-            away={match.score.fullTime.awayTeam}
-            status={match.status}
-          />
-          <AwayTeam>
-            <img src={teamsMap[match.awayTeam.id]['crestURL']} />
-            <span>{teamsMap[match.awayTeam.id]['short']}</span>
-          </AwayTeam>
-        </MatchContainer>
-      )}
-    </ScheduleContainer>
-  </Fragment>;
+const MatchdaySchedule = ({ matchday, matches, changeMatchday }) => {
+  const listedDates = [];
+  return (
+    <Fragment>
+      <MatchdayTitle>
+        {
+          matchday > 1 &&
+          <FontAwesomeIcon
+            onClick={changeMatchday(matchday - 1)}
+            icon={faChevronLeft}
+            size={"lg"} />
+        }
+        <span>{`Matchday ${matchday}`}</span>
+        {
+          matchday < 38 &&
+          <FontAwesomeIcon
+            onClick={changeMatchday(matchday + 1)}
+            icon={faChevronRight}
+            size={"lg"} />
+        }
+      </MatchdayTitle>
+      <ScheduleContainer>
+        {matches.map(match => {
+          let date = new Date(match.utcDate)
+            .toLocaleDateString(undefined, { weekday: 'long', month: 'long', day: 'numeric', year: 'numeric' })
+          if (listedDates.includes(date)) date = null;
+          else listedDates.push(date);
+          return (
+            <Fragment>
+              {date && <div>{date}</div>}
+              <MatchContainer key={match.id}>
+                <HomeTeam>
+                  <span>{teamsMap[match.homeTeam.id]['short']}</span>
+                  <img src={teamsMap[match.homeTeam.id]['crestURL']} />
+                </HomeTeam>
+                <ScoreContainer
+                  home={match.score.fullTime.homeTeam}
+                  away={match.score.fullTime.awayTeam}
+                  status={match.status}
+                />
+                <AwayTeam>
+                  <img src={teamsMap[match.awayTeam.id]['crestURL']} />
+                  <span>{teamsMap[match.awayTeam.id]['short']}</span>
+                </AwayTeam>
+              </MatchContainer>
+            </Fragment>
+          )
+        }
+        )}
+      </ScheduleContainer>
+    </Fragment>)
+};
 
 export default MatchdaySchedule;
