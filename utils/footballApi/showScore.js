e19b71d2cc77cbfac639268dc0a3abe594bc0942
John <jjh5166@gmail.com>
2020-12-05 21:54:41 -0500

    postponed match text

--- utils/footballApi/showScore.js
+++ utils/footballApi/showScore.js
@@ -1,6 +1,5 @@
 export default function showScore(status) {
-  const showIfStatus = ['FINISHED', 'AWARDED', 'IN_PLAY', 'PAUSED'];
-  const showIndex = showIfStatus.indexOf(status);
-  return [showIndex > -1, showIndex < 2];
-  //return array [showScore?, isLive?]
+  const showIfStatus = ['FINISHED', 'AWARDED', 'IN_PLAY', 'PAUSED']
+  const showIndex = showIfStatus.indexOf(status)
+  return showIndex > -1
 }
