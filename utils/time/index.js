799e4a8ff6331777fbabbcfc666bf71b670f814d
John <jjh5166@gmail.com>
2020-12-06 22:59:39 -0500

    getTimeFromUtc

---
+++ utils/time/index.js
@@ -0,0 +1,5 @@
+export function getTimeFromUtc(utcDate) {
+  const date = new Date(utcDate);
+  return date.toLocaleTimeString().replace(/(:00)(?!.*\b\1\b)/, "");
+}
+// returns '7:00 PM', '12:30 PM', etc.
