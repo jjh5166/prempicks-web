52e3d75db24286e5e7294c8082792c983da40a14
John <jjh5166@gmail.com>
2020-12-08 22:13:55 -0500

    add Alerts in Layout

--- components/Layout/index.js
+++ components/Layout/index.js
@@ -1,11 +1,36 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Head from 'next/head';
+import { useDispatch } from 'react-redux';
+import Snackbar from '@material-ui/core/Snackbar';
+import MuiAlert from '@material-ui/lab/Alert';
+import Grow from '@material-ui/core/Grow';
 
+import { clearAlert } from '../../redux/actions/alert';
+import { useAlert } from '../../redux/hooks';
 import NavBar from './NavBar';
 import { PageContainer, ContentContainer } from './styled';
 
-const Layout = ({ children, title = "PremPicks", hideNav = false }) => {
 
+function Alert(props) {
+  return <MuiAlert elevation={6} variant="filled" {...props} />;
+}
+
+function GrowTransition(props) {
+  return <Grow {...props} />;
+}
+
+const Layout = ({ children, title = "PremPicks", hideNav = false }) => {
+  const dispatch = useDispatch();
+  const { message, severity } = useAlert();
+  const [open, setOpen] = useState(false);
+
+  const handleClose = () => {
+    setOpen(false);
+    dispatch(clearAlert());
+  };
+  useEffect(() => {
+    setOpen(message ? true : false);
+  }, [message])
   return (
     <PageContainer>
       <Head>
@@ -15,6 +40,17 @@ const Layout = ({ children, title = "PremPicks", hideNav = false }) => {
       </Head>
       {!hideNav && <NavBar />}
       <ContentContainer>
+        <Snackbar
+          anchorOrigin={{ vertical: 'top', horizontal: 'center' }}
+          autoHideDuration={3000}
+          open={open}
+          onClose={handleClose}
+          TransitionComponent={GrowTransition}
+        >
+          <Alert onClose={handleClose} severity={severity}>
+            {message}
+          </Alert>
+        </Snackbar>
         {children}
       </ContentContainer>
     </PageContainer>
