95e37f453c71cf76062b63ee61ee89fdbfacf729
John <jjh5166@gmail.com>
2020-12-16 11:46:38 -0500

    PickField comments

--- components/MyPicks/Fields/PickField.js
+++ components/MyPicks/Fields/PickField.js
@@ -5,10 +5,11 @@ import { teamChoices } from '../../../constants';
 
 import { DisabledTeamsContext } from '../Context';
 
-const teams = ["", ...teamChoices];
+const teams = ["", ...teamChoices]; //allow for blank selection
 
 const PickField = ({ name, disabled }) => {
   const disabledTeams = useContext(DisabledTeamsContext);
+  // use to disable selection for teams taken in past matchdays
   return (
     <Field
       name={name}
