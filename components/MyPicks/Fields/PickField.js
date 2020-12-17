e94c5844cf9f0d683be299540ef663655150ac03
John <jjh5166@gmail.com>
2020-12-16 21:51:32 -0500

    highlight duplicate selections

--- components/MyPicks/Fields/PickField.js
+++ components/MyPicks/Fields/PickField.js
@@ -2,24 +2,29 @@ import { useContext } from 'react';
 import { Field } from 'formik';
 
 import { teamChoices } from '../../../constants';
-
-import { DisabledTeamsContext } from '../Context';
-
+import { SelectWithHighlight } from './styled';
+import { DisabledTeamsContext, NewSelectedTeamsContext } from '../Context';
+import { checkArrayDupes } from '../../../utils/checkArrayDupes';
 const teams = ["", ...teamChoices]; //allow for blank selection
 
 const PickField = ({ name, disabled }) => {
-  const disabledTeams = useContext(DisabledTeamsContext);
-  // use to disable selection for teams taken in past matchdays
+  const disabledTeams = useContext(DisabledTeamsContext) // use to disable selection for teams taken in past matchdays
+  const newlySelectedTeams = useContext(NewSelectedTeamsContext) // use to check for teams selected more than once
   return (
     <Field
       name={name}
       type="select"
-      as="select"
-      disabled={disabled}
     >
-      {teams.map(t => {
-        return <option key={t} value={t} disabled={disabledTeams.includes(t)}>{t}</option>;
-      })}
+      {  ({ field }) =>
+        <SelectWithHighlight
+          {...field}
+          disabled={disabled}
+          isError={checkArrayDupes(newlySelectedTeams, field.value)}
+        >
+          {teams.map(t => {
+            return <option key={t} value={t} disabled={disabledTeams.includes(t)}>{t}</option>
+          })}
+        </SelectWithHighlight>}
     </Field>
   );
 };
--- components/MyPicks/Fields/index.js
+++ components/MyPicks/Fields/index.js
@@ -2,7 +2,7 @@ import { useContext } from 'react';
 import { FieldArray } from 'formik';
 import PickField from './PickField';
 
-import { MyPicksContext, DisabledTeamsContext } from '../Context';
+import { MyPicksContext, DisabledTeamsContext, NewSelectedTeamsContext } from '../Context';
 import { FieldsGrid, FieldBlock } from './styled';
 
 const MyPicksFields = ({ values }) => {
@@ -11,11 +11,13 @@ const MyPicksFields = ({ values }) => {
   const slices = showHalf === 1 ? [0, 19] : [19, 38];
   const indexFactor = showHalf === 1 ? 0 : 19;
   const disabledTeams = []
+  const newSelectedTeams = []
   return (
     <div>
       <FieldArray name="picks">
         <FieldsGrid>
           <DisabledTeamsContext.Provider value={disabledTeams}>
+            <NewSelectedTeamsContext.Provider value={newSelectedTeams}>
           {
             values.picks ? (
               values.picks.slice(slices[0], slices[1]).map((pick, index) => {
@@ -23,6 +25,8 @@ const MyPicksFields = ({ values }) => {
                 const fieldDisabled = timeNow > scheduleData.schedule[pickIndex + 1][0]['utcDate']
                 if (fieldDisabled){
                   disabledTeams.push(pick.team_id)
+                } else if (pick.team_id){
+                  newSelectedTeams.push(pick.team_id)
                 }
                 return (
                   <FieldBlock key={`pick${pickIndex}`}>
@@ -35,6 +39,7 @@ const MyPicksFields = ({ values }) => {
                 );
               })) : null
           }
+          </NewSelectedTeamsContext.Provider>
           </DisabledTeamsContext.Provider>
         </FieldsGrid>
       </FieldArray>
