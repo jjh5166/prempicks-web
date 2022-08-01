a75cc93ca5d4a826ad9f94803156a8a0d494cadf
John <jjh5166@gmail.com>
2022-08-01 15:54:07 +0100

    user update email, team_name; disable name fields

--- components/UserForm/Update/formConfig.js
+++ components/UserForm/Update/formConfig.js
@@ -1,39 +1,42 @@
-import axios from 'axios';
+import axios from 'axios'
 
-import { serverUrl } from '../../../constants';
-import { setErrorAlert, setSuccessAlert } from '../../../redux/actions/alert';
+import { serverUrl } from '../../../constants'
+import { setErrorAlert, setSuccessAlert } from '../../../redux/actions/alert'
 
 export const updateFields = [
   {
     name: 'first_name',
     labelName: 'First Name',
-    isPassword: false
+    isPassword: false,
+    disabled: true,
   },
   {
     name: 'last_name',
     labelName: 'Last Name',
-    isPassword: false
+    isPassword: false,
+    disabled: true,
   },
+  { name: 'email', labelName: 'Email', isPassword: false },
   {
     name: 'team_name',
     labelName: 'Team Name',
-    isPassword: false
-  }
-];
+    isPassword: false,
+  },
+]
 
 export const updateAccountFn = async (firebase, data, dispatch) => {
-  await axios.put(`${serverUrl}/v1/user`, {
-    user: {
-      first_name: data.first_name,
-      last_name: data.last_name,
-      team_name: data.team_name
-    },
-    idToken: await firebase.retrieveToken()
-  },
-    { headers: { 'Content-Type': 'application/json' } })
-    .then(() =>
-      dispatch(setSuccessAlert('Account Updated'))
-    ).catch(() =>
-      dispatch(setErrorAlert('There was an error. Try Again.'))
-    );
-};
+  await axios
+    .put(
+      `${serverUrl}/v1/user`,
+      {
+        user: {
+          team_name: data.team_name,
+          email: data.email,
+        },
+        idToken: await firebase.retrieveToken(),
+      },
+      { headers: { 'Content-Type': 'application/json' } }
+    )
+    .then(() => dispatch(setSuccessAlert('Account Updated')))
+    .catch(() => dispatch(setErrorAlert('There was an error. Try Again.')))
+}
