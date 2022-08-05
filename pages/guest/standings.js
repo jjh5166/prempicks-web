d14e5eef17700b5f7f566594be4c62d1758f9474
John <jjh5166@gmail.com>
2022-08-05 14:38:25 +0100

    missing import

--- pages/guest/standings.js
+++ pages/guest/standings.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect, Fragment } from 'react'
 import { useDispatch } from 'react-redux'
+import Loader from 'react-loader-spinner'
 
 import { updateMatchday, initGuestStandings } from '../../redux/thunks'
 import { useGuestUser } from '../../redux/hooks'
@@ -7,8 +8,10 @@ import { pluckPicks } from '../../utils/guest'
 import Layout from '../../components/Layout'
 import StandingsTable from '../../components/Tables/userStandings'
 import GuestInfo from '../../components/GuestInfo'
+
 const informationText = `The Standings table displays picks in descending order so the most recent matchday is shown first 
 throughout the season. This data has been randomly generated. If you skip ahead your picks will be made automatically.`
+
 export default function GuestStandingsPage() {
     const [isLoading, setIsLoading] = useState(false)
     const [standings, setStandings] = useState(null)
