58b34bad15aec67e57b203b2be088120ff14fe20
John <jjh5166@gmail.com>
2023-07-27 20:13:07 +0100

    comment out data fetching

--- pages/standings.js
+++ pages/standings.js
@@ -1,26 +1,26 @@
 import { useState, useEffect } from 'react'
-import { useDispatch } from 'react-redux'
+// import { useDispatch } from 'react-redux'
 import Router from 'next/router'
 import axios from 'axios'
 import Loader from 'react-loader-spinner'
 
-import { setSuccessAlert } from 'redux/actions/alert'
-import { triggerScoring } from '../utils/footballApi'
+// import { setSuccessAlert } from 'redux/actions/alert'
+// import { triggerScoring } from '../utils/footballApi'
 import {
     serverUrl,
-    footballApiKey,
-    footballApiBaseUrl,
-    teamsMap,
+    // footballApiKey,
+    // footballApiBaseUrl,
+    // teamsMap,
 } from '../constants'
 import useAuthUser from '../redux/hooks/useAuthUser'
 import StandingsTable from '../components/Tables/userStandings'
 import { TeamListTable } from 'components/Tables/TeamListTable'
 
-export default function StandingsPage({ matchData }) {
+export default function StandingsPage() {
     const [isLoading, setIsLoading] = useState(false)
     const [standings, setStandings] = useState(null)
-    const [scores, setScores] = useState()
-    const dispatch = useDispatch()
+    // const [scores, setScores] = useState()
+    // const dispatch = useDispatch()
     const authUser = useAuthUser()
 
     useEffect(() => {
@@ -39,7 +39,7 @@ export default function StandingsPage({ matchData }) {
                 .then(res => {
                     setStandings(res.data)
                     setIsLoading(false)
-                    setScores(res.data.scores)
+                    // setScores(res.data.scores)
                 })
                 .catch(err => console.log(err.response))
         }
@@ -48,27 +48,27 @@ export default function StandingsPage({ matchData }) {
         }
     }, [authUser])
 
-    useEffect(() => {
-        if (scores) {
-            /* if there are matches, check whether home team has been scored for the matchday.
-            if not, init scoring job on the server */
-            const matchCheck = matchData.map(({ matchday, homeTeam }) => [
-                teamsMap[homeTeam.id]['abv'],
-                matchday,
-            ])
-            try {
-                const scoreThis = matchCheck.find(
-                    check => scores[check[1]][check[0]] === 0
-                )
-                if (scoreThis) {
-                    triggerScoring(scoreThis[1])
-                    dispatch(setSuccessAlert('Updating Scores...'))
-                }
-            } catch {
-                console.log('Error checking scores')
-            }
-        }
-    }, [scores])
+    // useEffect(() => {
+    //     if (scores) {
+    //         /* if there are matches, check whether home team has been scored for the matchday.
+    //         if not, init scoring job on the server */
+    //         const matchCheck = matchData.map(({ matchday, homeTeam }) => [
+    //             teamsMap[homeTeam.id]['abv'],
+    //             matchday,
+    //         ])
+    //         try {
+    //             const scoreThis = matchCheck.find(
+    //                 check => scores[check[1]][check[0]] === 0
+    //             )
+    //             if (scoreThis) {
+    //                 triggerScoring(scoreThis[1])
+    //                 dispatch(setSuccessAlert('Updating Scores...'))
+    //             }
+    //         } catch {
+    //             console.log('Error checking scores')
+    //         }
+    //     }
+    // }, [scores])
     return (
         <>
             {isLoading ? (
@@ -88,21 +88,21 @@ export default function StandingsPage({ matchData }) {
     )
 }
 
-export async function getServerSideProps() {
-    const today = new Date().toISOString().slice(0, 10)
-    const yestDate = new Date()
-    yestDate.setDate(yestDate.getDate() - 7)
-    const yesterday = yestDate.toISOString().slice(0, 10)
+// export async function getServerSideProps() {
+//     const today = new Date().toISOString().slice(0, 10)
+//     const yestDate = new Date()
+//     yestDate.setDate(yestDate.getDate() - 7)
+//     const yesterday = yestDate.toISOString().slice(0, 10)
 
-    const response = await axios.get(
-        // request matches from yesterday to today
-        `${footballApiBaseUrl}/competitions/2021/matches?dateFrom=${yesterday}&dateTo=${today}&status=FINISHED`,
-        { headers: { 'X-Auth-Token': footballApiKey } }
-    )
-    console.log('server props test', response.data.matches)
-    return {
-        props: {
-            matchData: response.data.matches,
-        },
-    }
-}
+//     const response = await axios.get(
+//         // request matches from yesterday to today
+//         `${footballApiBaseUrl}/competitions/2021/matches?dateFrom=${yesterday}&dateTo=${today}&status=FINISHED`,
+//         { headers: { 'X-Auth-Token': footballApiKey } }
+//     )
+//     console.log('server props test', response.data.matches)
+//     return {
+//         props: {
+//             matchData: response.data.matches,
+//         },
+//     }
+// }
