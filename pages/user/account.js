1eff62511c2d6f0d2e5822e360672c9f0211c3e0
John <jjh5166@gmail.com>
2023-07-30 13:11:48 +0100

    userdata initial values from context

--- pages/user/account.js
+++ pages/user/account.js
@@ -1,53 +1,18 @@
-import { useEffect, useState } from 'react'
+import { useEffect } from 'react'
 import Router from 'next/router'
-import axios from 'axios'
 
-import useAuthUser from '../../redux/hooks/useAuthUser'
-import { serverUrl } from '../../constants'
-import UpdateAccountForm from '../../components/UserForm/Update'
-import { LoadingIndicator } from 'components/LoadingIndicator'
-
-const initialValues = {
-    first_name: '',
-    last_name: '',
-    team_name: '',
-}
+import UpdateAccountForm from 'components/UserForm/Update'
+import { useCurrentUser } from 'context/currentUser'
 
 const UpdateAccountPage = () => {
-    const authUser = useAuthUser()
-    const [isLoading, setIsLoading] = useState(false)
-    const [userData, setUserData] = useState(initialValues)
+    const { idToken, currentUser } = useCurrentUser()
+
     useEffect(() => {
-        if (!authUser) {
-            Router.push('/user/login')
+        if (!idToken || !currentUser) {
+            Router.push('/')
         }
-        const fetchData = async () => {
-            setIsLoading(true)
-            await axios
-                .get(
-                    `${serverUrl}/v1/user`,
-                    { params: { idToken: authUser.idToken } },
-                    { headers: { 'Content-Type': 'application/json' } }
-                )
-                .then(res => {
-                    setUserData(res.data)
-                    setIsLoading(false)
-                })
-                .catch(err => console.log(err.response))
-        }
-        if (authUser) {
-            fetchData()
-        }
-    }, [authUser])
-    return (
-        <>
-            {isLoading ? (
-                <LoadingIndicator />
-            ) : (
-                <UpdateAccountForm initialValues={userData} />
-            )}
-        </>
-    )
+    }, [])
+    return <UpdateAccountForm initialValues={currentUser} />
 }
 
 export default UpdateAccountPage
