c7ea5a48a118e1a3ea84c63b874dc504e71c4c0a
John <jjh5166@gmail.com>
2023-08-06 14:16:49 +0100

    add tiebreakers, esc all text

--- components/Rules/index.js
+++ components/Rules/index.js
@@ -1,6 +1,6 @@
 import Tooltip from '@material-ui/core/Tooltip'
 
-import { TextContainer } from '../Container'
+import { TextContainer } from 'components/Container'
 import RulesTable from './RulesTable'
 import { RulesSection } from './styled'
 
@@ -10,61 +10,61 @@ export default function Rules() {
     return (
         <TextContainer>
             <RulesSection>
-                <h4>How To Play</h4>
+                <h4>{`How To Play`}</h4>
                 <p>
-                    Every week, you pick a team to win. Points are received or
+                    {`Every week, you pick a team to win. Points are received or
                     deducted based on the result. The season is divided into two
                     halves, you cannot pick the same team twice in a half. There
                     are 19 matches in each half of the season, meaning one team
                     goes unpicked each half. The Top 3 in each half and total
                     season get a pay out. There is also a payout for longest win
-                    streak.
+                    streak.`}
                 </p>
             </RulesSection>
             <RulesSection>
-                <h4>Scoring</h4>
+                <h4>{`Scoring`}</h4>
                 <div>
                     <RulesTable />
                     <ul>
                         <li>
-                            Goal Difference Factor: If the team you pick wins by
+                            {`Goal Difference Factor:  If the team you pick wins by
                             3 or more goals, you receive a bonus point. If the
                             team you pick loses by 3 or more goals, you are
-                            penalized an additional point.{' '}
+                            penalized an additional point.`}
                         </li>
                         <li>
-                            Top 6 Loss: Any time a{' '}
+                            {`Top 6 Loss: Any time a `}
                             <Tooltip title={topSix}>
                                 <span>
-                                    <strong>top 6 team</strong>
+                                    <strong>{`top 6 team`}</strong>
                                 </span>
-                            </Tooltip>{' '}
-                            from last season is picked and they lose, there is
-                            an additional point deduction.
+                            </Tooltip>
+                            {` from last season is picked and they lose, there is
+                            an additional point deduction.`}
                         </li>
                         <li>
-                            Beat a Top Six: Any time the team you select beats a{' '}
+                            {`Beat a Top Six: Any time the team you select beats a `}
                             <Tooltip title={topSix}>
                                 <span>
-                                    <strong>top 6 team</strong>
+                                    <strong>{`top 6 team`}</strong>
                                 </span>
-                            </Tooltip>{' '}
-                            from last season you will receive a bonus point
+                            </Tooltip>
+                            {` from last season you will receive a bonus point`}
                         </li>
                         <li>
-                            Newly Promoted Win: If you pick any of the{' '}
+                            {`Newly Promoted Win: If you pick any of the `}
                             <Tooltip title={newlyPromoted}>
                                 <span>
-                                    <strong>newly promoted teams</strong>
+                                    <strong>{`newly promoted teams`}</strong>
                                 </span>
-                            </Tooltip>{' '}
-                            and they win, you will receive a bonus point.
+                            </Tooltip>
+                            {` and they win, you will receive a bonus point.`}
                         </li>
                     </ul>
                 </div>
             </RulesSection>
             <RulesSection>
-                <h4>Picking</h4>
+                <h4>{`Picking`}</h4>
                 <p>
                     {`Picks must be in by kick off of the first match of the
                     matchday, otherwise the selection will be auto-picked.
@@ -75,6 +75,30 @@ export default function Rules() {
                     matchday and points will be tallied once the match is
                     completed.`}
                 </p>
+            </RulesSection>
+            <RulesSection>
+                <h4>{`Tie Breakers`}</h4>
+                <p>
+                    {`In the event that there is a tie in the standings for either half of the season, higher finish with be determined by:`}
+                </p>
+                <ol>
+                    <li>
+                        {`Most points from `}
+                        <Tooltip title={newlyPromoted}>
+                            <span>
+                                <strong>{`newly promoted teams`}</strong>
+                            </span>
+                        </Tooltip>
+                    </li>
+                    <li>
+                        {`Most points against a `}
+                        <Tooltip title={topSix}>
+                            <span>
+                                <strong>{`top 6 from last season`}</strong>
+                            </span>
+                        </Tooltip>
+                    </li>
+                </ol>
             </RulesSection>
         </TextContainer>
     )
