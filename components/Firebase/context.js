190624802c3abd193409b41d8edc49d80bccdeef
John <jjh5166@gmail.com>
2023-08-06 13:57:38 +0100

    refresh idToken on page visible

--- components/Firebase/context.js
+++ components/Firebase/context.js
@@ -2,6 +2,7 @@ import { useEffect, createContext, useContext } from 'react'
 
 import Firebase from './firebase'
 import { useCurrentUser } from 'context/currentUser'
+import { useRef } from 'react'
 
 const FirebaseContext = createContext(null)
 
@@ -12,13 +13,29 @@ export const useFirebase = () => {
 export function FirebaseProvider({ children }) {
     const firebase = new Firebase()
     const { setCurrentUser, setIdToken } = useCurrentUser()
+    const hasRunFocusLogic = useRef(false)
+
+    const handleVisibilityChange = () => {
+        if (!document.hidden && !hasRunFocusLogic.current) {
+            // Page has become visible, and we haven't run the focus logic since it was last hidden
+            refreshIdToken()
+            hasRunFocusLogic.current = true
+        } else if (document.hidden) {
+            // Page has become hidden, so we can reset the flag
+            hasRunFocusLogic.current = false
+        }
+    }
+
+    const refreshIdToken = async () => {
+        const idToken = await firebase.retrieveToken()
+        setIdToken(idToken)
+    }
     useEffect(() => {
         // Listen authenticated user
         const unsubscriber = firebase.auth.onAuthStateChanged(async user => {
             try {
                 if (user) {
-                    const idToken = await firebase.retrieveToken()
-                    setIdToken(idToken)
+                    refreshIdToken()
                 } else {
                     setCurrentUser(null)
                     setIdToken(null)
@@ -31,6 +48,25 @@ export function FirebaseProvider({ children }) {
 
         // Unsubscribe auth listener on unmount
         return () => unsubscriber()
+    }, [])
+
+    useEffect(() => {
+        if (typeof window !== 'undefined') {
+            window.addEventListener(
+                'visibilitychange',
+                handleVisibilityChange,
+                false
+            )
+            window.addEventListener('focus', handleVisibilityChange, false)
+        }
+
+        return () => {
+            window.removeEventListener(
+                'visibilitychange',
+                handleVisibilityChange
+            )
+            window.removeEventListener('focus', handleVisibilityChange)
+        }
     }, [])
 
     return (
